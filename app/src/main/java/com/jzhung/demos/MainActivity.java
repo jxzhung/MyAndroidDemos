1a0b9cf657ed39fd4d822abcf31fc9825da91b40
jzhung <i@jzhung.com>
2016-08-08 16:26:29 +0800

    scale anim test

--- app/src/main/java/com/jzhung/demos/MainActivity.java
+++ app/src/main/java/com/jzhung/demos/MainActivity.java
@@ -1,42 +1,55 @@
 package com.jzhung.demos;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Typeface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-import com.jzhung.demos.view.customview.MaoBiTextView;
-
-import java.util.List;
-import java.util.Map;
+import com.jzhung.demos.anim.viewanimation.AnimationActivity;
 
 public class MainActivity extends Activity {
-    private ListView mListView;
+    private LinearLayout contentLL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        mListView = (ListView) findViewById(R.id.lv);
-//        mListView.setAdapter();
-
-        /*MaoBiTextView mbTv = (MaoBiTextView) findViewById(R.id.mbTv);
-        mbTv.setText("风一样的男人");
-        mbTv.setTextSize(50);
-        Typeface fontFace = Typeface.createFromAsset(getAssets(),
-                "font/huawenxingkai.ttf");
-        mbTv.setTypeface(fontFace);*/
-
+        findViews();
+        initBtns();
     }
 
-    class ListAdapter extends SimpleAdapter{
-
-        public ListAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
-            super(context, data, resource, from, to);
-        }
-
-
+    private void initBtns() {
+        addButton("缩放动画", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity(AnimationActivity.class);
+            }
+        });
     }
+
+    private void findViews() {
+        contentLL = (LinearLayout) findViewById(R.id.contentLL);
+    }
+
+    private void addButton(String text, View.OnClickListener onClick) {
+        Button btn = new Button(this);
+        btn.setText(text);
+        btn.setOnClickListener(onClick);
+        contentLL.addView(btn);
+    }
+
+    private void openActivity(Class target){
+        startActivity(new Intent(this, target));
+    }
+
+//    private void textUseCustomeFont(){
+//        MaoBiTextView mbTv = (MaoBiTextView) findViewById(R.id.mbTv);
+//        mbTv.setText("风一样的男人");
+//        mbTv.setTextSize(50);
+//        Typeface fontFace = Typeface.createFromAsset(getAssets(),
+//                "font/huawenxingkai.ttf");
+//        mbTv.setTypeface(fontFace);
+//    }
 }
--- app/src/main/java/com/jzhung/demos/anim/viewanimation/AnimationActivity.java
+++ app/src/main/java/com/jzhung/demos/anim/viewanimation/AnimationActivity.java
@@ -2,6 +2,10 @@ package com.jzhung.demos.anim.viewanimation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import com.jzhung.demos.R;
 
@@ -11,5 +15,15 @@ public class AnimationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+
+        //缩放动画
+        final Animation scaleAnim = AnimationUtils.loadAnimation(this, R.anim.scale);
+        TextView animTv = (TextView) findViewById(R.id.animTv);
+        animTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(scaleAnim);
+            }
+        });
     }
 }

8b78c2d84d65688b35ff249030afdc04c999c3d8
Jzhung <i@jzhung.com>
2016-08-09 22:46:05 +0800

    add myapplication

---
+++ app/src/main/java/com/jzhung/demos/MyApplication.java
@@ -0,0 +1,14 @@
+package com.jzhung.demos;
+
+import android.app.Application;
+
+/**
+ * Created by jzhung on 16/8/9.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
